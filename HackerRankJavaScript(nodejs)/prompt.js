cf6f6c962dc558c7a9ac8ac11973a2d44c57d87e
bepul0 <bepul.cse.ru@gmail.com>
2020-03-27 16:40:14 +0600

    Take user input from console by using prompt-sync

---
+++ HackerRankJavaScript(nodejs)/prompt.js
@@ -0,0 +1,9 @@
+const prompt = require('prompt-sync')();
+
+const name = prompt('What is your name?');
+console.log(`Hey there ${name}`);
+
+const num=Number(prompt('enter a number '));
+for(let i=0; i<num; i++){
+    console.log('i = '+i+' '+(num+5.5));
+}
