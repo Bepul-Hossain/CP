d9ece697433238302d93943c8343da94b15ec555
bepul0 <bepul.cse.ru@gmail.com>
2020-03-28 18:55:26 +0600

    change of folder structure

---
+++ HackerRankJavaScript(nodejs)/10 Days of JavaScript/test.js
@@ -0,0 +1,5 @@
+var clickMeButton = document.createElement('button');
+clickMeButton.id = 'myButton';
+clickMeButton.innerHTML = 'Click Me';
+clickMeButton.style.background = '#4FFF8F';
+document.body.appendChild(clickMeButton);
