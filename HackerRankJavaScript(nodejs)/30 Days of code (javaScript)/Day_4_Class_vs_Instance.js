5a16d4678917dafb012a2fe05a1dbb164630c530
bepul0 <bepul.cse.ru@gmail.com>
2020-03-29 22:28:18 +0600

    Day 4 complete

--- HackerRankJavaScript(nodejs)/30 Days of code (javaScript)/Day_4_Class_vs_Instance.js
+++ HackerRankJavaScript(nodejs)/30 Days of code (javaScript)/Day_4_Class_vs_Instance.js
@@ -1,34 +1,26 @@
-const prompt = require("prompt-sync")({ sigint: true });
-
-function Person(initialAge) {
-  if (initialAge < 0) {
+function Person(initialAge){
+    // Add some more code to run some checks on initialAge
+    if (initialAge < 0) {
     console.log("Age is not valid, setting age to 0.");
     initialAge = 0;
   }
+  //var age;
+  //console.log(age)
   this.age = initialAge;
-  console.log(this.age);
 
-  this.amIOld = function() {
-    if (age < 13) {
+  this.amIOld=function(){
+   // Do some computations in here and print out the correct statement to the console
+    if (this.age < 13) {
       console.log("You are young.");
-    } else if (age >= 13 && age < 18) {
+    } else if (this.age >= 13 && this.age < 18) {
       console.log("You are a teenager.");
     } else {
       console.log("You are old.");
     }
-  };
-  this.yearPasses = function() {
-    this.age++;
-  };
-}
 
-const T = prompt();
-var p = new Person();
-for (let i = 0; i < T; i++) {
-  var age = prompt();
-  for (let j = 0; j < 3; j++) {
-    Person(age);
-    p.yearPasses();
-  }
-  p.amIOld();
+  };
+   this.yearPasses=function(){
+          // Increment the age of the person in here
+          this.age++;
+   };
 }
