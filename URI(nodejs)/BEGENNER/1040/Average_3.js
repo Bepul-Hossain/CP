58305c5ed3eadabdc7fc533249ba063f7bcca128
bepul0 <bepul.cse.ru@gmail.com>
2020-04-04 16:57:52 +0600

    wrong answer(5%)

--- URI(nodejs)/BEGENNER/1040/Average_3.js
+++ URI(nodejs)/BEGENNER/1040/Average_3.js
@@ -1,11 +1,11 @@
-var input = require("fs").readFileSync("/dev/stdin", "utf8");
-const lines = input.split(" ");
-
-const N1 = 2 * parseFloat(Number(lines[0]));
-const N2 = 3 * parseFloat(Number(lines[1]));
-const N3 = 4 * parseFloat(Number(lines[2]));
-const N4 = parseFloat(Number(lines[3]));
-var average = parseFloat((Number(N1 + N2 + N3 + N4) / 10).toFixed(1));
+var input = require("fs").readFileSync("../dev/file.txt", "utf8");
+const linesF = input.split('\n');
+const lines =linesF[0].split(" ");
+const N1 = 2 * parseFloat(lines[0]);
+const N2 = 3 * parseFloat(lines[1]);
+const N3 = 4 * parseFloat(lines[2]);
+const N4 = parseFloat(lines[3]);
+var average = parseFloat((N1 + N2 + N3 + N4) / 10).toFixed(1);
 
 console.log("Media: " + average);
 if (average >= 7.0) {
@@ -14,13 +14,17 @@ if (average >= 7.0) {
   console.log("Aluno reprovado.");
 } else if (average >= 5.0 && average <= 6.9) {
   console.log("Aluno em exame.");
-  const lines2 = parseFloat(Number(input));
+  const lines2 = parseFloat(linesF[1]);
   console.log("Nota do exame: " + lines2);
-  average = (average + lines2) / 2;
-  if (average >= 5.0) {
+  
+   var p = parseFloat(average);
+   var media=(p+lines2)/2
+
+  
+  if (media >= 5.0) {
     console.log("Aluno aprovado.");
   } else {
     console.log("Aluno reprovado.");
   }
-  console.log("Media final: " + average.toFixed(1));
+  console.log("Media final: " + parseFloat(media).toFixed(1));
 }
