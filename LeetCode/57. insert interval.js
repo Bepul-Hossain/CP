b3bf9f141a8cde0db6d7d706dbf4db902903217b
bepul-sharetrip <bepul@sharetrip.net>
2023-12-17 11:39:30 +0600

    interval

--- LeetCode/57. insert interval.js
+++ LeetCode/57. insert interval.js
@@ -1,38 +1,27 @@
 var insert = function (intervals, newInterval) {
     let res = [];
     let n = intervals.length;
-    if (n === 0) return newInterval;
-    let flag = false;
+    let flag = true;
     for (let i = 0; i < n; i++) {
 
-        const interval = intervals[i];
-        // console.log(i);
-        if (newInterval[0] >= interval[0] && newInterval[0] <= interval[1]) {
-            newInterval[0] = interval[0];
-            flag = true;
-            continue;
-        }
-        else if (newInterval[1] >= interval[0] && newInterval[1] <= interval[1]) {
-            newInterval[1] = interval[1];
-            flag = true;
-            continue;
-        }
-        else if (interval[0] >= newInterval[0] && interval[0] <= newInterval[1]) {
-            continue;
-        }
-        else if (interval[1] >= newInterval[0] && interval[1] <= newInterval[1]) {
-            continue;
-        }
-        if (flag === false) {
-            res.push(interval);
-        } else {
-            res.push(newInterval);
-            res.push(...intervals.slice(i));
+        if (newInterval[1] < intervals[i][0]) {
+            res.push(newInterval, ...intervals.slice(i));
+            flag = false;
             break;
         }
 
+        else if (newInterval[0] > intervals[i][1]) {
+            res.push(intervals[i])
+        }
+
+        else {
+            newInterval = [Math.min(newInterval[0], intervals[i][0]), Math.max(newInterval[1], intervals[i][1])];
+        }
+    }
+    if (flag) {
+        res.push(newInterval);
     }
     return res;
 };
 
-console.log(insert([[1, 3], [6, 9]], [2, 5]));
+console.log(insert([[2, 5], [6, 7], [8, 9]], [0, 1]));
