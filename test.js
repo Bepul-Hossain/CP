4e90b8f4d82f93c0b83c13599c3c6a2283ee76dd
bepul-sharetrip <bepul@sharetrip.net>
2023-12-21 12:00:15 +0600

    linked list

--- test.js
+++ test.js
@@ -1,11 +1,48 @@
-function rec(num, sum = 0) {
-    if (num == 0) {
-        console.log(sum);
-        return sum;
-    }
-    else {
-        return rec(num - 1, sum + num);
+let list =
+{
+    value: 1,
+    next: {
+        value: 2,
+        next: {
+            value: 3,
+            next: {
+                value: 4,
+                next: {
+                    value: 5,
+                    next: null
+                }
+            }
+        }
     }
 }
 
-console.log(rec(10000));
+let dummyList = {
+    next: list
+};
+
+let head = dummyList;
+let left = 2;
+
+for (let i = 1; i < left; i++) {
+    head = head.next;
+}
+
+// console.log(JSON.stringify(head));
+let right = 4;
+let curr = head.next;
+// console.log(curr);
+let next = null;
+let prev = null;
+
+for (let i = left; i < right; i++) {
+
+    next = curr.next;
+    curr.next = next.next;
+    next.next = head.next;
+    head.next = next;
+}
+
+
+console.log(JSON.stringify(dummyList.next));
+// console.log(JSON.stringify(prev));
+// console.log(JSON.stringify(list));
